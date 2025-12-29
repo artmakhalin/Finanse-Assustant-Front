a979eb9356ca07f42de96e6ec4216d58ab841eb1
artmakhalin <artmakhalin@gmail.com>
2025-12-28 23:03:31 -0500

    categories edit and delete added

--- assets/js/pages/accounts.page.js
+++ assets/js/pages/accounts.page.js
@@ -24,6 +24,7 @@ const btnLogout = document.getElementById("btnLogout");
 const addAccountForm = document.getElementById("createAccountForm");
 const addCategoryForm = document.getElementById("createCategoryForm");
 const editAccountForm = document.getElementById("editAccountForm");
+const editCategoryForm = document.getElementById("editCategoryForm");
 
 const categoryOpeners = document.querySelectorAll("[data-opener-id]");
 
@@ -37,6 +38,9 @@ const alertBoxModalCreateCategory = document.getElementById(
 const alertBoxModalEditAccount = document.getElementById(
   "alertBoxModalEditAccount"
 );
+const alertBoxModalEditCategory = document.getElementById(
+  "alertBoxModalEditCategory"
+);
 
 const accountsDiv = document.getElementById("accountsDiv");
 const incomeDiv = document.getElementById("incomeDiv");
@@ -45,6 +49,7 @@ const expensesDiv = document.getElementById("expensesDiv");
 const addAccountModal = document.getElementById("addAccountModal");
 const editAccountModal = document.getElementById("editAccountModal");
 const addCategoryModal = document.getElementById("addCategoryModal");
+const editCategoryModal = document.getElementById("editCategoryModal");
 
 //State
 let accountsCache = [];
@@ -54,6 +59,7 @@ const categoriesByType = {
   TRANSFER: [],
 };
 let editingAccountId = null;
+let editingCategoryId = null;
 let currentCategoryType = null;
 
 //Start Page
@@ -72,6 +78,7 @@ function bindEvents() {
   addAccountForm.addEventListener("submit", onCreateAccountSubmit);
   editAccountForm.addEventListener("submit", onEditAccountSubmit);
   addCategoryForm.addEventListener("submit", onCreateCategorySubmit);
+  editCategoryForm.addEventListener("submit", onEditCategorySubmit);
 
   for (const btn of categoryOpeners) {
     btn.addEventListener("click", onCategoryOpenerClick);
@@ -184,6 +191,16 @@ function bindEvents() {
     }
   }
 
+  async function onEditCategorySubmit(e) {
+    e.preventDefault;
+    if (!editingCategoryId || !currentCategoryType) return;
+    const currentCategory = categoriesByType[currentCategoryType].find(
+      (cat) => cat.categoryId == editingCategoryId
+    );
+
+    await updateCategory(currentCategory);
+  }
+
   function onCategoryOpenerClick(e) {
     currentCategoryType = e.currentTarget.dataset.openerId;
   }
@@ -218,17 +235,35 @@ function bindEvents() {
   }
 
   function onCategoriesClick(e) {
+    currentCategoryType = e.target.dataset.openerId;
+
+    if (!currentCategoryType) {
+      showAlert(
+        alertBoxModalCreateCategory,
+        "danger",
+        "Choose category type first"
+      );
+      return;
+    }
+
     const editBtn = e.target.closest("button.category-edit");
     const deleteBtn = e.target.closest("button.category-delete");
 
     if (deleteBtn) {
-      // TODO: deleteCategory(deleteBtn.dataset.id)
-      return;
+      return deleteCategory(deleteBtn.dataset.id);
     }
 
     if (editBtn) {
-      // TODO: open edit modal + set editingCategoryId
-      return;
+      alertBoxModalEditCategory.innerHTML = "";
+      const id = editBtn.dataset.id;
+
+      const currentCategory = categoriesByType[currentCategoryType].find(
+        (cat) => cat.categoryId == id
+      );
+      editingCategoryId = id;
+
+      const newDescriptionInput = document.getElementById("newDescription");
+      newDescriptionInput.value = currentCategory.description;
     }
   }
 }
@@ -260,6 +295,27 @@ async function loadCategories() {
   } catch (err) {
     showApiError(err, alertBox, {
       fallback: "Error when loading categories",
+      fieldLabels,
+    });
+  }
+}
+
+async function deleteCategory(id) {
+  try {
+    await request(`${API_CATEGORIES}/${id}`, { method: "DELETE" });
+    showAlert(alertBox, "success", "Category removed");
+    categoriesByType[currentCategoryType] = categoriesByType[
+      currentCategoryType
+    ].filter((cat) => cat.categoryId != id);
+
+    renderCategories(
+      categoriesByType[currentCategoryType],
+      currentCategoryType
+    );
+    currentCategoryType = null;
+  } catch (err) {
+    showApiError(err, alertBox, {
+      fallback: "Error when deleting categories",
       fieldLabels,
     });
   }
@@ -310,10 +366,47 @@ async function updateAccount(currentAccount) {
 
     showAlert(alertBox, "success", "Account updated");
     editAccountForm.reset();
+    editingAccountId = null;
     hideModal(editAccountModal);
   } catch (err) {
     showApiError(err, alertBoxModalEditAccount, {
       fallback: "Error when updating account",
+      fieldLabels,
+    });
+  }
+}
+
+async function updateCategory(currentCategory) {
+  try {
+    const id = currentCategory.categoryId;
+    const type = currentCategory.type;
+    const fd = new FormData(editCategoryForm);
+
+    //TODO - fix API
+    const payLoad = {
+      id: id,
+      description: fd.get("newDescription")?.trim(),
+      type: type,
+    };
+
+    const updatedCategory = await request(`${API_CATEGORIES}/${id}`, {
+      method: "PUT",
+      body: payLoad,
+    });
+
+    categoriesByType[type] = categoriesByType[type].map((cat) =>
+      cat.categoryId == id ? updatedCategory : cat
+    );
+    renderCategories(categoriesByType[type], type);
+
+    showAlert(alertBox, "success", "Category updated");
+    editCategoryForm.reset();
+    editingCategoryId = null;
+    currentCategoryType = null;
+    hideModal(editCategoryModal);
+  } catch (err) {
+    showApiError(err, alertBoxModalEditCategory, {
+      fallback: "Error when updating category",
       fieldLabels,
     });
   }
@@ -352,8 +445,10 @@ function renderCategories(categories, type) {
         title: c.description,
         deleteClass: "category-delete",
         editClass: "category-edit",
+        editTarget: "#editCategoryModal",
         titleClass: "card-title mb-0 text-truncate-2",
         bodyClass: "card-body py-3 d-flex flex-column",
+        type: type,
       }),
     `You don't have any ${typeLabels[type]}`
   );
@@ -408,31 +503,31 @@ function el(tag, { className, text, attrs, dataset } = {}, children = []) {
   if (text != null) node.textContent = text;
 
   if (attrs) {
-    for (const [k, v] of Object.entries(attrs)) node.setAttribute(k, v);
+    for (const [k, v] of Object.entries(attrs)) if (k) node.setAttribute(k, v);
   }
 
   if (dataset) {
-    for (const [k, v] of Object.entries(dataset)) node.dataset[k] = v;
+    for (const [k, v] of Object.entries(dataset)) if (k) node.dataset[k] = v;
   }
 
   for (const child of children) node.appendChild(child);
   return node;
 }
 
-function createActions({ id, deleteClass, editClass, editTarget } = {}) {
+function createActions({ id, deleteClass, editClass, editTarget, type } = {}) {
   const actions = el("div", { className: "d-flex gap-2 flex-shrink-0" });
 
   const btnRemove = el("button", {
     className: `btn btn-outline-danger btn-sm rounded-3 action-btn ${deleteClass}`,
     text: "X",
-    dataset: { id },
+    dataset: { id: id, openerId: type },
     attrs: { type: "button" },
   });
 
   const btnEdit = el("button", {
     className: `btn btn-outline-secondary btn-sm rounded-3 action-btn ${editClass}`,
     text: "...",
-    dataset: { id },
+    dataset: { id, openerId: type },
     attrs: { type: "button" },
   });
 
@@ -455,13 +550,20 @@ function buildCard({
   deleteClass,
   editClass,
   editTarget, // например "#editAccountModal"
+  type,
 } = {}) {
   const head = el("div", {
     className: "d-flex align-items-start justify-content-between gap-2",
   });
 
   const h5 = el("h5", { className: titleClass, text: title });
-  const actions = createActions({ id, deleteClass, editClass, editTarget });
+  const actions = createActions({
+    id,
+    deleteClass,
+    editClass,
+    editTarget,
+    type,
+  });
 
   head.append(h5, actions);
 
